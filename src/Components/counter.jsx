abcdbd721c2d1a4191f9213281c0d64c7a313ae9
anjlosanchez <anjlosanchez@gmail.com>
2021-06-26 13:14:09 +0800

    commit1

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,18 +2,13 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["Tag1", "tag3", "tag3"],
+    id: this.props.counter.id,
+    value: this.props.counter.value,
+    onDelete: this.props.onDelete,
   };
 
-  constructor() {
-    super();
-
-    this.handleIncrement.bind(this);
-  }
-
   handleIncrement = (product) => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -26,18 +21,24 @@ class Counter extends Component {
         >
           Increment
         </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
-
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count == 0 ? "warning" : "primary";
+    classes += this.state.value == 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
 
     return count == 0 ? "Zero" : count;
   }
---
+++ src/Components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 4 },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  handleDelete = (id) => {
+    let counters = this.state.counters.filter((n) => n.id != id);
+    this.setState({ counters });
+  };
+}
+
+export default Counters;
